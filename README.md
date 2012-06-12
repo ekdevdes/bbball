5a4c1bb861b3395905942d1ca612c6d4fe2c9453
Ethan Kramer <ekdevdes@gmail.com>
2012-06-11 22:48:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,130 @@
+bbball 
+======
+A simple and powerful PHP Client that covers every endpoint of the dribbble api
+
+Installation
+------------
+
+Open terminal and paste the following:
+
+`cd the-directory-to-put-bbball-in`
+
+`git clone https://github.com/ekdevdes/bbball.git`
+
+Usage
+-----
+
+```php
+<?php
+  
+ include_once 'bbball.php';
+ 
+ $dribbble = new BBBall;
+ 
+ // fetch page 2
+ $dribbble->page = 2;
+ 
+ // …and lets get 20 shots per page
+ $dribbble->per_page = 20;
+
+ // get shots 
+ $shots = $dribbble->get_shots();
+ 
+ // get a shot
+ $shot = $dribbble->get_shot(43424);
+ 
+ // get a player's info
+ $player_info = $dribbble->get_player_info('ethankr');
+ 
+ // get only a shot's rebounds
+ $rebounds = $dribbble->get_shot_rebounds(43424);
+ 
+ // get only a shot's comments
+ $comments = $dribbble->get_shot_comments(43424);
+ 
+```
+
+Documentation
+-------------
+
+After you've included the `bbball.php` file and create a new instance of it here are the methods you can use:
+
++  `get_shot($id,$show_rebounds=false,$show_comments=false)`
+	+ **Gets a single shot from dribbbble**
+	+ `$id` `int` The shots id. [Required] [Default: none]
+	+ `$show_rebounds` `bool` If true will include the given shot's (the shot is specified by the $id param) rebounds [Optional][Default: false]
+	+ `$show_comments` `bool` If true will include the given shot's (the shot is specified by the $id param) comments [Optional][Default: false] 
+	+ ** Returns: an array of the parsed form of the resulting JSON **
+	
++  `get_shots($list="everyone",$player_id=NULL)`
+	+ ** Gets shots from dribbble with optionally, a few filters **
+	+ `$list` `string` The list to fetch the shots from. Either "debuts","everyone","popular","following" or "likes" [Optional][Default: "everyone"]. For "following" or "likes" the `$player_id` must be not NULL 
+	+ `$player_id` `int` A given player's id. If not present then will it will be ignored. If present $list will be ignored. [Optional][Default: none]
+	<br>
+	  NOTE: In order to specify the "following" or "likes" options the `$player_id` must not be NULL
+	  <br>
+	 NOTE: `$player_id` can either the players actual id or their userame without the '@' in the begining	 
+	 + ** Returns: an array of the parsed form of the resulting JSON **
++ `get_player_info($id)`
+	+ **Get's a players info given a player id.**
+	+ `$id` `int` or `string` The player's id [Required][Default: none]. $id can either be the users actual id or their username
+	+ ** Returns: an array of the parsed form of the resulting JSON**
++ `get_shot_rebounds($id)`
+	+ ** Gets only a given shot's rebounds **
+	+ `$id` `int` the given shot's id
+	+ **Returns:  an array of the given shot's (specified by the `$id`) rebounds **
++ `get_shot_comments($id)`
+	+ ** Gets only a given shot's comments **
+	+ `$id` `int` the given shot's id
+	+ ** Returns: an array of the given shot's (specified by the `$id`) comments **
+	
+Bug Reporter & Feature Requests
+------------
+
+Please report any bugs here on Github. 
+
+[http://github.com/ekdevdes/bbball/issues](http://github.com/ekdevdes/bbball/issues)
+
+Thank you.
+
+Contributing
+------------
+
+If you would like to contribute to Storage.js please do the following:
+
++	Fork this repo
++	Make your changes
++	Document the changes using [PHPDoc](http://phpdoc.org)
++	Send me a pull request
+
+Thank you :)
+
+License
+-------
+
+bbball IS DOUBLE LICENSED UNDER THE MIT LICENSE AND GPL LICENSE
+
+MIT LICENSE
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+GPL LICENSE
+
+A simple and powerful PHP Dribbble client
+	Copyright (C) 2011  Ethan Kramer
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
---
+++ bbball.php
@@ -0,0 +1,281 @@
+<?php
+
+/**
+  * bbball
+  * A Simple, Yet Powerful Dribbble PHP Client
+  *
+  * @author Ethan Kramer <ethankr@comcast.net>
+  * @package A Dribbble PHP Client
+  * @copyright Ethan Kramer 2012
+  * @see http://github.com/ekdevdes/bbball
+  * @link http://dribbble.com/api
+  *
+  * @todo Add paging support
+  * @todo Add per_page support
+  * 
+  */
+class BBBall {
+	
+	/**
+	  * Dribbbble api base url 
+	  *
+	  * @var string
+      */
+	var $api_base = "http://api.dribbble.com/";
+	
+	/**
+	  * Page of results to request from dribbbble api
+	  *
+	  * @var int
+      */
+	var $page = 1;
+	
+	/**
+	  * Number of results to retrieve per page
+	  *
+	  * @var int
+      */
+	var $per_page = 15;
+
+	
+	/**
+	  * Gets a single shot from dribbbble
+	  * @access public
+	  *
+	  * @param int $id The shots id. [Required] [Default: none]
+	  * @param bool $show_rebounds If true will include the given shot's (the shot is specified by the $id param) rebounds [Optional][Default: false]
+	  * @param bool $show_comments If true will include the given shot's (the shot is specified by the $id param) comments [Optional][Default: false] 
+	  *
+	  * @return array the parsed form of the resulting JSON
+	  */
+	public function get_shot($id,$show_rebounds=false,$show_comments=false){
+		
+		$url_shot = $this->api_base."shots/".(string)$id;
+		$url_rebounds = "";
+		$url_comments = "";
+		
+		if ($show_rebounds) {
+			$url_rebounds .= $this->api_base."shots/".(string)$id."/rebounds";
+		}
+		
+		if($show_comments){
+			$url_comments .= $this->api_base."shots/".(string)$id."/comments";
+		}
+		
+		if (!$show_rebounds && !$show_comments) {
+			
+			/**
+			  * Default, both $show_comments and $show_rebounds are false
+			  */
+			 $result = objectToArray(json_decode($this->get_contents($url_shot)));
+			
+			return $result;
+			
+			
+		}else if(!$show_rebounds && $show_comments){
+			
+			/**
+			  * Don't $show_rebounds but $show_comments
+			  */
+			
+			$result_shot = objectToArray(json_decode($this->get_contents($url_shot)));
+			$result_comments = objectToArray(json_decode($this->get_contents($url_comments)));
+			
+			$result_shot['comments'] = $result_comments['comments'];
+			
+			return $result_shot;
+			
+		}else if($show_rebounds && $show_comments){
+			
+			/**
+			  * $show_rebounds and $show_comments
+			  */
+			
+				$result_shot = objectToArray(json_decode($this->get_contents($url_shot)));
+				$result_comments = objectToArray(json_decode($this->get_contents($url_comments)));
+				$result_rebounds = objectToArray(json_decode($this->get_contents($url_rebounds)));
+
+				$result_shot['comments'] = $result_comments['comments'];
+				$result_shot['rebounds'] = $result_rebounds;
+				
+				return $result_shot;
+			
+		}else if($show_rebounds && !$show_comments){
+			
+			/**
+			  * $show_rebounds but not $show_comments
+			  */
+			
+			$result_shot = objectToArray(json_decode($this->get_contents($url_shot)));
+			$result_rebounds = objectToArray(json_decode($this->get_contents($url_rebounds)));
+
+			$result_shot['rebounds'] = $result_rebounds;
+			
+			return $result_shot;
+		}
+	
+	}
+	
+	/**
+	  * Gets shots from dribbble with optionally, a few filters
+	  * @access public
+	  *
+	  * @param string $list The list to fetch the shots from. Either "debuts","everyone","popular","following" or "likes" [Optional][Default: "everyone"]. For "following" or "likes"
+	  * 	   the $player_id must be not NULL 
+	  * @param int $player_id A given player's id. If not present then will it will be ignored. If present $list will be ignored. [Optional][Default: none]
+	  *		   NOTE: In order to specify the "following" or "likes" options the $player_id must not be NULL
+	  *  	   NOTE: $player_id can either the players actual id or their userame without the '@' in the begining
+	  *
+	  * @return array the parsed form of the resulting JSON
+	  */
+	public function get_shots($list="everyone",$player_id=NULL){
+		if (is_null($player_id) && $list == "following" || is_null($player_id) && $list == "likes") {
+			 throw new Exception(" Player id can't be null when requesting shots from the \"$list\" filter.");
+		}
+		
+		if ($list != "following" && $list != "likes") {
+			
+			switch ($list) {
+				case 'everyone':
+					
+					return objectToArray(json_decode($this->get_contents($this->api_base."shots/".$list."?page=".$this->page."&per_page=".$this->per_page)));
+					
+					break;
+				
+				case 'debuts':
+					
+					return objectToArray(json_decode($this->get_contents($this->api_base."shots/".$list."?page=".$this->page."&per_page=".$this->per_page)));
+					
+					break;
+				
+				case 'popular':
+					
+					return objectToArray(json_decode($this->get_contents($this->api_base."shots/".$list."?page=".$this->page."&per_page=".$this->per_page)));
+					
+					break;
+			}			
+			
+		}else if($list == "following" || $list == "likes"){
+			
+			switch ($list){
+				case "following":
+					return objectToArray(json_decode($this->get_contents($this->api_base."players/".$player_id."/shots/following?page=".$this->page."&per_page=".$this->per_page)));
+					break;
+				case "likes":
+					return objectToArray(json_decode($this->get_contents($this->api_base."players/".$player_id."/shots/likes?page=".$this->page."&per_page=".$this->per_page)));
+					break;	
+			}
+			
+		}
+	}
+	
+	/**
+	  * Get's a players info given a player id.
+	  * @access public 
+	  *
+	  * @param int|string $id The player's id [Required][Default: none]. $id can either be the users actual id or their username
+	  *
+	  * @return array the parsed form of the resulting JSON
+	  */
+	public function get_player_info($id){
+		if (is_null($id)) {
+			 throw new Exception("Can't get player info without a player id");
+		}else{
+			$base_url = $this->api_base."/players/".$id;
+			$following_url = $this->api_base."/players/".$id."/following?page=".$this->page."&per_page=".$this->per_page;
+			$followers_url = $this->api_base."/players/".$id."/followers?page=".$this->page."&per_page=".$this->per_page;
+			$draftees_url = $this->api_base."/players/".$id."/draftees?page=".$this->page."&per_page=".$this->per_page;
+			
+			$base_info = objectToArray(json_decode($this->get_contents($base_url)));
+			$following_info = objectToArray(json_decode($this->get_contents($following_url)));
+			$followers_info = objectToArray(json_decode($this->get_contents($followers_url)));
+			$draftees_info = objectToArray(json_decode($this->get_contents($draftees_url)));
+			
+			$base_info['followers'] = $followers_info['players'];
+			$base_info['following'] = $following_info['players'];
+			$base_info['draftees'] = $draftees_info['players'];
+			
+			return $base_info;
+			
+		}
+	}
+	
+	/**
+	  * Returns only a given shot's rebounds
+	  * @access public
+	  * 
+	  * @param int $id the given shot's id
+	  *
+	  * @return array the given shot's (specified by the $id) rebounds
+	  *
+	  */
+	public function get_shot_rebounds($id){
+		
+		if (is_null($id)) {
+		  throw new Exception("Can't get shot rebounds without a shot id");
+		}else{
+		  return objectToArray(json_decode($this->get_contents($this->api_base."/shots/$id/rebounds?page=".$this->page."&per_page=".$this->per_page)));
+		}
+		
+	}
+	
+	/**
+	  * Returns only a given shot's comments
+	  * @access public
+	  * 
+	  * @param int $id the given shot's id
+	  *
+	  * @return array the given shot's (specified by the $id) comments
+	  *
+	  */
+	public function get_shot_comments($id){
+		if (is_null($id)) {
+		  throw new Exception("Can't get shot comments without a shot id");
+		}else{
+		  return objectToArray(json_decode($this->get_contents($this->api_base."/shots/$id/comments?page=".$this->page."&per_page=".$this->per_page)));
+		}
+	}
+	
+	/**
+	  * Return the content of a url using curl 
+	  * @access private
+	  *
+	  * @param string $url the url which the get the contents from
+	  *
+	  * @return string a string representation the contents of the url
+	  * 
+	  */
+	 private function get_contents($url){
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+}
+
+/**
+   *
+   * Convert an object to an array
+   * @access public
+   *
+   * @param    Object  $object The object to convert
+   *
+   * @return   Array   an array representation of $object
+   *
+   */
+function objectToArray( $object )
+{
+   if( !is_object( $object ) && !is_array( $object ) )
+   {
+       return $object;
+   }
+   if( is_object( $object ) )
+   {
+       $object = get_object_vars( $object );
+   }
+   return array_map( 'objectToArray', $object );
+}
