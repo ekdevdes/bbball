f22139943695b546434ab5d9546b985ae4a8b7cd
Ethan Kramer <ekdevdes@gmail.com>
2012-07-18 22:51:57 -0400

    initial commit

---
+++ LICENSE.php
@@ -0,0 +1,34 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1172\cocoasubrtf100
+\cocoascreenfonts1{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
+
+\f0\fs24 \cf0 \
+bbball IS DOUBLE LICENSED UNDER THE MIT LICENSE AND GPL LICENSE\
+\
+MIT LICENSE\
+\
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\
+\
+GPL LICENSE\
+\
+A simple and powerful PHP Dribbble client\
+	Copyright (C) 2011  Ethan Kramer\
+\
+	This program is free software: you can redistribute it and/or modify\
+	it under the terms of the GNU General Public License as published by\
+	the Free Software Foundation, either version 3 of the License, or\
+	(at your option) any later version.\
+\
+	This program is distributed in the hope that it will be useful,\
+	but WITHOUT ANY WARRANTY; without even the implied warranty of\
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\
+	GNU General Public License for more details.\
+\
+	You should have received a copy of the GNU General Public License\
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.\
+\
+}
